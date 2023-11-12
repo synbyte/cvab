765c787b39f33c3c75ba56e3a989a9bbb181996c
synbyte <rashnikov2.0@gmail.com>
2023-11-12 01:36:36 +0000

    web report button

--- app/colors.tsx
+++ app/colors.tsx
@@ -14,11 +14,11 @@ export default function Colors() {
 
   return (
     <main className='border min-w-full border-orange-500 border-r-8 text-center text-white shadow-md rounded-lg p-3 bg-slate-600'>
-      <div>
+      <div >
         <p className='text-sm pb-3 underline'>Todays Colorline</p>
       {clValue}
-      
-        <ul><li> <a href="https://treatment-court.clark.wa.gov/?id=1">- - - - Web Report - - - -</a></li>
+     
+        <ul><li> <a href="https://treatment-court.clark.wa.gov/?id=1"><button className="outline outline-1 bg-orange-500 hover:bg-orange-700 text-white py-1 my-3 px-1 rounded">Web Report</button></a></li>
         </ul>
       </div>
     </main>

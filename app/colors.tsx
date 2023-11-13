0b8b78d1006dd83a64c952c0170447fb22887d11
synbyte <rashnikov2.0@gmail.com>
2023-11-13 02:08:44 +0000

    fixed color

--- app/colors.tsx
+++ app/colors.tsx
@@ -5,29 +5,25 @@ import { useEffect, useState } from 'react';
 
 import cheerio from 'cheerio';
 
-export default function Colors() {
-  const [clValue, setClValue] = useState('');
+async function getData() {
+  const response = await fetch("https://lifelineconnections.org/color-line/",{mode: 'no-cors'});
+  const content = await response.text();
+  const $ = cheerio.load(content);
+  const item = $('div.c-line').text();
+  return item;
+}
+export default async function Colors() {
+ 
+  const c = await getData();
 
-  useEffect( () => {
-    let cl;
-    async function fetchColors() {
-      const response = await fetch("https://lifelineconnections.org/color-line/",{mode: 'no-cors'});
-      const content = await response.text();
-      const $ = cheerio.load(content);
-      const item = $('div.c-line').text();
-      setClValue(item);
-     
-    };
-    
-    fetchColors();
-  },[]);
+  
  
 
   return (
     <main className='border min-w-full border-orange-500 border-r-8 text-center text-white shadow-md rounded-lg p-3 bg-slate-600'>
       <div >
         <p className='text-2xl text-orange-200 pb-3 font-bold'>Todays Colorline</p>
-      <p className='font-bold'>{clValue}</p>
+      <p className='font-bold'>{c}</p>
      
         <ul><li> </li>
         </ul>

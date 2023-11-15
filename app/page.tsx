1fd1e4dc6d58d4c692fcd7e6e3e114af802d3634
synbyte <rashnikov2.0@gmail.com>
2023-11-15 05:12:35 +0000

    removed use client from page.tsx, adds 3 errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 import Image from 'next/image'
 import Colors from '../components/Colors';
 import Links from '../components/Links';
@@ -9,9 +9,9 @@ export default async function Home() {
 
 
   return (
-    <main className="items-center p-5 min-w-full min-h-screen bg-gradient-to-tr  from-slate-700 to-slate-900">
+    <main className="items-center p-5 min-w-full min-h-screen bg-gradient-to-tr from-slate-700 to-slate-900">
       <p className='text-center text-white'>All things Drug Court in one place</p>
-      <div className='grid mx-auto md:grid-cols-2 md:w-5/6 gap-4'> 
+      <div className='grid gap-4 mx-auto md:grid-cols-2 md:w-5/6'> 
         <Colors />
 
         <Links />
--- components/Colors.tsx
+++ components/Colors.tsx
@@ -3,7 +3,14 @@
 import cheerio from 'cheerio';
 
 async function getColors(): Promise<string|null> {
-  const response = await fetch("https://lifelineconnections.org/color-line/", { next: { revalidate: 3600 } });
+  const response = await fetch("https://lifelineconnections.org/color-line/", { 
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'text/html'
+    },
+    next: { revalidate: 3600 }
+  });
   const content = await response.text();
   const $ = cheerio.load(content);
   const item = $('.cl-line-container').html(); console.log('Colors fetched: ', $('.cl-line-container div').text());
@@ -23,7 +30,7 @@ export default async function Colors() {
       <main className='p-3 text-center text-white rounded-2xl border border-r-8 border-orange-500 shadow-md colors bg-slate-600'>
         <div >
           <p className='pb-3 text-2xl font-bold text-orange-200 heading'>Colorline</p>
-          <p dangerouslySetInnerHTML={{ __html: await getColors() ?? 'No Colors/Closed'}}></p>
+          <p dangerouslySetInnerHTML={{ __html: await getColors() ?? ''}}></p>
 
           <ul><li> </li>
           </ul>

8f38bf674c22a400f57cf20933ad192ddaa51069
synbyte <rashnikov2.0@gmail.com>
2024-03-23 22:18:12 +0000

    changed background blur

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default  function Home() {
 
 
   return (
-    <main className="items-center p-5 min-w-full min-h-screen bg-gradient-to-tr from-slate-700 to-orange-300">
+    <main className="items-center p-5 min-w-full min-h-screen bg-gradient-to-tr from-slate-700 to-orange-300 pt-20">
       
       <div className='grid gap-4 mx-auto md:grid-cols-2 md:w-5/6'> 
         <Colors />

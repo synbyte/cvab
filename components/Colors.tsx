b89f63a37d656735a0c43206c7a31467c3ac2b76
synbyte <rashnikov2.0@gmail.com>
2024-03-24 20:36:55 +0000

    added banner

--- components/Colors.tsx
+++ components/Colors.tsx
@@ -26,9 +26,11 @@ export default async function Colors() {
   return (
     <>
 
-      <main className='p-3 text-center text-white rounded-2xl     bg-slate-600 shadow-md shadow-black hover:shadow-sm hover:shadow-black transition '>
+      <main className='p-3 text-center text-white rounded-2xl  bg-slate-600 shadow-md shadow-black hover:shadow-sm hover:shadow-black transition '>
         <div className=''>
           <p className='pb-3 text-2xl font-bold text-orange-200 heading'>Colorline</p>
+          <div className='bg-red-300 outline outline-red-600 rounded text-red-900 mb-3'><p><strong>Make sure to refresh page to update colors!</strong></p></div> 
+          
           <div className=" font-bold w-5/6 mx-auto" dangerouslySetInnerHTML={{ __html: await getColors() ?? ''}}></div>
           
           <ul><li> </li>

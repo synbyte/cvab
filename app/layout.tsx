467afcce15b83a0aeb0a2ced3ca296893106db47
synbyte <rashnikov2.0@gmail.com>
2024-07-22 22:20:48 +0000

    added cyan as body background

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <body className={inter.className}>
+      <body className={`${inter.className}`}>
         <nav className='fixed top-0  fixed w-full py-3 shadow-md shadow-gray-900 bg-cyan-800 rounded-b-2xl border-b-2 border-cyan-500'><ul className='nav text-cyan-100 text-lg space-x-5'><li className='transition hover:-translate-y-0.5'><a  id='link'href="/">Home</a></li ><li className='transition hover:-translate-y-0.5'><a id='link' href="/forms">Forms</a></li><li className='transition hover:-translate-y-0.5 text-orange-300'><a id='link' href="https://treatment-court.clark.wa.gov/?id=1">Web Report</a></li></ul> </nav>
         <div className=''>{children}</div></body>
     </html>

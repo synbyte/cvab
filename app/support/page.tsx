d03e7ff9d1b93074e97ebf1dd509abe8de1b04bf
synbyte <rashnikov2.0@gmail.com>
2025-01-22 19:18:40 +0000

    Added HA, some styling changes, typeo fixes

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,16 +1,16 @@
 export default async function Home() {
   return (
     <main className="items-center p-5 min-w-full min-h-screen  pt-20">
-      <div className="p-3 mx-auto text-center rounded-2xl shadow-md shadow-black bg-cyan-50/70 backdrop-blur-md md:w-5/6">
+      <div className="p-3 mx-auto text-center rounded-2xl shadow-md shadow-black bg-cyan-50/70 backdrop-blur-md w-full">
         <p className="pb-4 text-2xl font-bold text-cyan-800 heading">Support</p>
         <div className="md:grid md:grid-cols-2">
-          <div className="flex flex-col">
+          <div className="flex flex-col mx-3">
             <ul className="text-left text-cyan-700 md:text-left">
               <a href="https://cvabonline.com">
                 <p className="font-bold text-cyan-800 underline">Pier 360</p>
               </a>
-              <p className="text-xs">West - (360)397-8050</p>
-              <p className="text-xs">East - (360)253-4036</p>
+              <p className="text-xs">West - (360) 397-8050</p>
+              <p className="text-xs">East - (360) 253-4036</p>
               <li>
                 <a href="https://www.cvabonline.org/s/Pier360-West-January-2025-Final.pdf">
                   West center calendar
@@ -28,7 +28,7 @@ export default async function Home() {
               </li>
               <li>
                 <a href="https://www.cvabonline.org/training">
-                  Pier 360 academy
+                  Pier 360 Academy
                 </a>
               </li>
               <li>
@@ -40,7 +40,7 @@ export default async function Home() {
               <a href="https://www.recoverycafecc.org/">
                 <p className="font-bold text-cyan-800 underline">Recovery Cafe</p>
               </a>
-              <p className="text-xs">(360)984-6163</p>
+              <p className="text-xs">(360) 984-6163</p>
               <li>
                 <a href="https://www.recoverycafecc.org/about-3-1">Calendar</a>
               </li>
@@ -56,7 +56,7 @@ export default async function Home() {
               <a href="https://couvecollective.org/">
                 <p className="font-bold text-cyan-800 underline">Couve Collective</p>
               </a>
-              <p className="text-xs">(564)208-4842</p>
+              <p className="text-xs">(564) 208-4842</p>
               <li>
                 <a href="https://couvecollective.org/events/">
                   Calendar
@@ -66,25 +66,25 @@ export default async function Home() {
               <li><a href="https://couvecollective.org/recovery/">Membership</a></li>
             </ul>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col mx-3">
            
             <ul className="text-left text-cyan-700 md:text-left">
               <a href="https://www.restoredandrevived.com/">
                 <p className="font-bold text-cyan-800 underline">Restored & Revived</p>
               </a>
-              <p className="text-xs">(541)350-2098</p>
+              <p className="text-xs">(541) 350-2098</p>
               <li>
                 <a href="https://www.restoredandrevived.com/events">
                   Calendar
                 </a>
               </li>
-              <li><a href="https://www.restoredandrevived.com/programs-and-services">Programs and Services</a></li>
+              <li><a href="https://www.restoredandrevived.com/programs-and-services">Programs & Services</a></li>
             </ul>
             <ul className="text-left text-cyan-700 md:text-left">
               <a href="https://www.vancouveraa.org/">
                 <p className="font-bold text-cyan-800 underline">AA</p>
               </a>
-              <p className="text-xs">(360)314-4619</p>
+              <p className="text-xs">(360) 314-4619</p>
               <li>
                 <a href="https://www.vancouveraa.org/events/">
                   Calendar
@@ -92,13 +92,13 @@ export default async function Home() {
               </li>
               <li><a href="https://www.vancouveraa.org/meetings/">Meetings</a></li>
               <li><a href="https://www.vancouveraa.org/links/">Links & Resources</a></li>
-              <li><a href="https://www.restoredandrevived.com/programs-and-services">Programs and Services</a></li>
+              <li><a href="https://www.restoredandrevived.com/programs-and-services">Programs & Services</a></li>
             </ul>
             <ul className="text-left text-cyan-700 md:text-left">
               <a href="https://swanaonline.org/">
                 <p className="font-bold text-cyan-800 underline">NA</p>
               </a>
-              <p className="text-xs">(360)703-0990</p>
+              <p className="text-xs">(360) 703-0990</p>
               <li>
                 <a href="https://swanaonline.org/events/">
                   Calendar
@@ -107,10 +107,22 @@ export default async function Home() {
               <li><a href="https://swanaonline.org/meetings/">Meetings</a></li>
             </ul>
             <ul className="text-left text-cyan-700 md:text-left">
+              <a href="https://heroinanonymous.org/">
+                <p className="font-bold text-cyan-800 underline">HA</p>
+              </a>
+              
+              <li>
+                <a href="https://heroinanonymous.org/resources/">
+                  Resources
+                </a>
+              </li>
+              <li><a href="https://heroinanonymous.org/meetings1/">Meetings</a></li>
+            </ul>
+            <ul className="text-left text-cyan-700 md:text-left">
               <a href="https://smartrecovery.org/">
                 <p className="font-bold text-cyan-800 underline">Smart Recovery</p>
               </a>
-              <p className="text-xs">(440)951-5357</p>
+              <p className="text-xs">(440) 951-5357</p>
               <li>
                 <a href="https://swanaonline.org/events/">
                   Calendar
--- components/Finder.tsx
+++ components/Finder.tsx
@@ -1,6 +1,19 @@
+import Link from "next/link";
+
 export default function Finder() {
-    return(
-        <div className=" md:w-5/6 mx-auto my-5  text-center  text-white shadow-sm shadow-black rounded-2xl p-3 bg-cyan-50/70 backdrop-blur-md"><p className='pb-3 text-2xl font-bold text-cyan-800 heading'>AA Group Finder</p><iframe className="shadow-md rounded-2xl shadow-black" frameBorder="0" height="600px" width="100%" src="https://www.vancouveraa.org/meetings/" ></iframe></div>
-   
-    )
+  return (
+    <div className=" md:w-5/6 mx-auto my-5  text-center  text-white shadow-sm shadow-black rounded-2xl p-3 bg-cyan-50/70 backdrop-blur-md">
+      <p className=" text-2xl font-bold text-cyan-800 heading">
+        AA Group Finder
+      </p>
+      <p className="text-sm text-cyan-800 pb-3">(Other meetings can be found on <Link className="font-bold underline" href="/support">Groups/Resources</Link> page.)</p>
+      <iframe
+        className="shadow-md rounded-2xl shadow-black"
+        frameBorder="0"
+        height="600px"
+        width="100%"
+        src="https://www.vancouveraa.org/meetings/"
+      ></iframe>
+    </div>
+  );
 }

6d2294c7d9167a9455ca16e7fc835f31147cda83
synbyte <rashnikov2.0@gmail.com>
2025-09-07 17:17:29 -0700

    updated pier360 calendar to september

--- app/data/supportData.js
+++ app/data/supportData.js
@@ -7,7 +7,7 @@ export const supportData = {
         east: "(360) 253-4036"
       },
       links: [
-        { href: "https://www.pier360.org/_files/ugd/a5e4e2_fae3d17ba10549c4b541d894a15f3578.pdf", text: "Center calendar" },
+        { href: "https://www.pier360.org/_files/ugd/a5e4e2_62d6338bfc8141a2adcfb7aa7bf554d9.pdf", text: "Center calendar" },
         { href: "https://www.pier360.org/services", text: "Information on services provided" },
         { href: "https://www.pier360.org/academy", text: "Pier 360 Academy" },
         { href: "https://www.pier360.org/warmline", text: "Warm Line" }

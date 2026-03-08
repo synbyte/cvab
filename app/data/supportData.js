0a5672021ebcc7b4f2c9152a436c5337737546d4
synbyte <81550106+synbyte@users.noreply.github.com>
2026-03-07 19:11:33 -0800

    Update calendar link in supportData.js

--- app/data/supportData.js
+++ app/data/supportData.js
@@ -7,7 +7,7 @@ export const supportData = {
         east: "(360) 253-4036"
       },
       links: [
-        { href: "https://8c02e614-d9af-4505-8905-f2a6e6d2bd53.filesusr.com/ugd/a5e4e2_3a5ee7563aec4189beba23b93f9bbabe.pdf", text: "Center calendar" },
+        { href: "https://8c02e614-d9af-4505-8905-f2a6e6d2bd53.filesusr.com/ugd/a5e4e2_3c38776d1dcb4f29984b6d02ac69ed49.pdf", text: "Center calendar" },
         { href: "https://www.pier360.org/services", text: "Information on services provided" },
         { href: "https://www.pier360.org/academy", text: "Pier 360 Academy" },
         { href: "https://www.pier360.org/warmline", text: "Warm Line" }
